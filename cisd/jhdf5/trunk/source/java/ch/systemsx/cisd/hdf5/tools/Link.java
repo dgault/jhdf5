cd2998afa1dd536589befc7c241132600ac0df6c
brinn <brinn@b4b7471a-772a-0410-ab70-ea09ad806966>
2009-03-29 23:07:10 +0000

    add: interfaces IHDF5Reader and IHDf5Writer refactor: replace HDF5Reader by IHDF5Reader and HDF5Writer with IHDF5Writer refactor: rename HDF5SimpleReader to IHDF5SimpleReader refactor: rename HDf5SimpleWriter to IHDF5SimpleWriter

--- cisd/jhdf5/trunk/source/java/ch/systemsx/cisd/hdf5/tools/Link.java
+++ cisd/jhdf5/trunk/source/java/ch/systemsx/cisd/hdf5/tools/Link.java
@@ -27,7 +27,7 @@ import ch.systemsx.cisd.hdf5.HDF5EnumerationType;
 import ch.systemsx.cisd.hdf5.HDF5EnumerationValue;
 import ch.systemsx.cisd.hdf5.HDF5LinkInformation;
 import ch.systemsx.cisd.hdf5.HDF5ObjectType;
-import ch.systemsx.cisd.hdf5.HDF5Reader;
+import ch.systemsx.cisd.hdf5.IHDF5Reader;
 
 /**
  * A class containing all information we need have about a link either in the file sysstem or in an
@@ -185,7 +185,7 @@ public final class Link implements Comparable<Link>
      * 
      * @return The new start position in the concatenated names.
      */
-    int initAfterReading(String concatenatedNames, int startPos, HDF5Reader reader,
+    int initAfterReading(String concatenatedNames, int startPos, IHDF5Reader reader,
             String groupPath, boolean readLinkTarget)
     {
         try
